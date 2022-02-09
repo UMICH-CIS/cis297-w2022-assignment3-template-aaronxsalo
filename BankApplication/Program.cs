36ffb78c629fee75e2ea0a24ca84578d286fdb55
aaronxsalo <60741625+aaronxsalo@users.noreply.github.com>
2022-02-09 10:58:32 -0500

    Updated Class Structure and Fixed Build Errors
    Moved classes to program.cs because I was having issues with header.cs. Fixed issues with inheritance on build.

--- BankApplication/Program.cs
+++ BankApplication/Program.cs
@@ -10,9 +10,96 @@ namespace BankApplication
     {
         static void Main(string[] args)
         {
-            SavingsAccount sa = new SavingsAccount(1000.0m,0.15m);
+            SavingsAccount save = new SavingsAccount(1000.0m,0.01m);
+			CheckingAccount check = new CheckingAccount(1000.0m,0.50m);
 
             Console.ReadLine();
         }
     }
+
+    public abstract class Account
+	{
+		private decimal accountBalance;
+
+		public decimal AccountBalance { get { return accountBalance; } } 
+
+		public Account(decimal bal)
+        {
+			if(bal >= 0.0m)
+            {
+				accountBalance = bal;
+			}
+			else
+            {
+				Console.WriteLine("ERROR! Initial account balance must be at least 0.");
+            }
+        }
+
+		public virtual void Credit(decimal amt)
+        {
+			accountBalance += amt;
+        }
+
+		public virtual bool Debit(decimal amt)
+        {
+			if(amt <= accountBalance)
+            {
+				accountBalance -= amt;
+				return true;
+            }
+			else
+            {
+				Console.WriteLine("ERROR! Debit amount exceeded the account balance.");
+				return false;
+            }
+        }
+	}
+
+	public class SavingsAccount : Account
+	{
+		private decimal interestRate;
+
+		public SavingsAccount(decimal bal, decimal intr) : base(bal)
+        {
+			interestRate = intr;
+        }
+
+		public decimal CalculateInterest()
+        {
+			return interestRate * this.AccountBalance;
+        }
+	}
+
+	public class CheckingAccount : Account
+	{
+		private decimal transFee;
+
+		public CheckingAccount(decimal bal, decimal fee) : base(bal)
+        {
+			transFee = fee;
+        }
+
+		public override void Credit(decimal amt)
+        {
+			base.Credit(amt);
+			base.Debit(transFee);
+		}
+
+		public override bool Debit(decimal amt)
+        {
+			if (amt <= this.AccountBalance)
+			{
+				if(base.Debit(amt))
+                {
+					base.Debit(transFee);
+				}
+				return true;
+			}
+			else
+			{
+				Console.WriteLine("ERROR! Debit amount exceeded the account balance.");
+				return false;
+			}
+		}
+	}
 }
